ede6b4c25d1d5aa9d9cd4b8f93fbdb9aad8792c2
Md. Jillur Rahman <105236203+jillur984@users.noreply.github.com>
2024-10-19 11:58:48 +0600

    Simple Backend Basic Level Practise

---
+++ index.js
@@ -0,0 +1,38 @@
+const express=require('express')
+const bodyParser=require('body-parser')
+const app=express()
+const PORT=3000;
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get("/",(req,res)=>{
+   res.sendFile(__dirname+"/index.html")
+})
+app.get("/circle",(req,res)=>{
+    res.sendFile(__dirname+"/circle.html")
+ })
+ app.get("/triangle",(req,res)=>{
+    res.sendFile(__dirname+"/triangle.html")
+ })
+
+ app.post("/triangle",(req,res)=>{
+   const base=req.body.base
+   const height=req.body.height
+   const area=0.5*base*height
+   res.send(`<h2>Trangle area is ${area}</h2>`)
+ })
+ app.post("/circle",(req,res)=>{
+    const radius=req.body.radius
+    
+    const area=Math.PI * radius * radius
+    res.send(`Circle area is ${area}`)
+  })
+
+app.listen(PORT,()=>{
+    console.log(`Server is Running at http://localhost:${PORT}`)
+})
+
+
